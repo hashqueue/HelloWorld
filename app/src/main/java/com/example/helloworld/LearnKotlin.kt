bfb140adb60979bf22c1257cc8db64bf4a053d78
hashqueue <1912315910@qq.com>
2021-08-19 00:13:44 +0800

    学习使用var和val在kotlin中声明变量

---
+++ app/src/main/java/com/example/helloworld/LearnKotlin.kt
@@ -0,0 +1,14 @@
+package com.example.helloworld
+
+fun main() {
+    println("Hello Kotlin!")
+    // 使用var关键字创建可变变量
+    val a = 10
+    println("a = $a")
+    //显示声明变量类型为Int类型
+    var b: Int = 20
+    b = 40
+    println("b = $b")
+    val b1 = b * 10
+    println("b1 = $b1")
+}
