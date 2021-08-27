97b00aff3b1bacc859020b3c987173e2d8d79059
hashqueue <1912315910@qq.com>
2021-08-28 01:13:00 +0800

    新增扩展函数

---
+++ app/src/main/java/com/example/helloworld/LearnExtensionFunction.kt
@@ -0,0 +1,18 @@
+package com.example.helloworld
+
+object LearnExtensionFunction {
+    fun countLetters(str: String): Int {
+        var counter: Int = 0
+        for (item in str) {
+            if (item.isLetter()) {
+                counter += 1
+            }
+        }
+        return counter
+    }
+}
+
+fun main() {
+    val str = "ABC123xyz!@#"
+    println("字符串$str 中一共有${LearnExtensionFunction.countLetters(str = str)}个字母和数字.")
+}
